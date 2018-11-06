bb3200bcec51d23f526b0592a10388dc0c5eaeb8
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2018-11-06 12:09:44 +0100

    Primitive vs Objects

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ console.log(mark.lastName);
 
 /////////////////////////////
 // Lecture: Object.create
-
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -53,6 +53,49 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 });
+*/
+
+
+
+/////////////////////////////
+// Lecture: Primitives vs objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
 
 
 
