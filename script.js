50d99f31785b262e8bbb538a5017f68bffab8188
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2018-11-06 12:04:58 +0100

    Object create

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /////////////////////////////
 // Lecture: Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -30,5 +30,29 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+
+
+/////////////////////////////
+// Lecture: Object.create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
+
 
 
